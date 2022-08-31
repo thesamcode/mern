177234e8df7f060c43368f459ce2a3703d8ae8fb
thesamcode <stores1@digitalmail.one>
2022-08-31 16:19:39 -0700

    new updaes

---
+++ first_jsx_assignment/src/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 align="left">Hello Dojo!</h1>
+        <h2 align="left" >Things I need to do:</h2>
+        <list align="left">
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </list>
+      </div>
+  );
+}
+
+export default App;
