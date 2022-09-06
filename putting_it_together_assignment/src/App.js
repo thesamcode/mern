a039346c71618d659ab24ebd21d7322ea215e25e
thesamcode <stores1@digitalmail.one>
2022-09-06 15:43:47 -0700

    new updates

--- putting_it_together_assignment/src/App.js
+++ putting_it_together_assignment/src/App.js
@@ -4,23 +4,31 @@ import './App.css';
 
 import UserForm from './components/UserForm';
 
-var peopleArr =[
-  {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
-  {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
-  {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
-  {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
-]
+// var peopleArr =[
+//   {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
+//   {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
+//   {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
+//   {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
+// ]
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {peopleArr.map(person => {
+//         // these are the properties called "props" being sent into components: firstName, lastName, etc....
+//         return <UserForm firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
+//       })
+//       }
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div className="App">
-      {peopleArr.map(person => {
-        // these are the properties called "props" being sent into components: firstName, lastName, etc....
-        return <UserForm firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
-      })
-      }
-    </div>
-  );
+    return (
+        <div className="App">
+            <UserForm />
+        </div>
+    );
 }
 
 export default App;
--- putting_it_together_assignment/src/components/UserForm.js
+++ putting_it_together_assignment/src/components/UserForm.js
@@ -2,31 +2,49 @@
 import React, { useState } from  'react';
     
 const UserForm = (props) => {
-    const [username, setUsername] = useState("");
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
     const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");  
+    const [password, setPassword] = useState("");
+    const [confirmpw, setConfirmPW] = useState("");
     
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { username, email, password };
+        const newUser = { firstname, lastname, email, password, confirmpw };
         console.log("Welcome", newUser);
     };
     
     return(
         <form onSubmit={ createUser }>
             <div>
-                <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } value={ firstname } />
             </div>
             <div>
-                <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } value={ lastname } />
+            </div>
+            <div>
+                <label>Email: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } value={ email }/>
             </div>
             <div>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } value={ password } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="text" onChange={ (e) => setConfirmPW(e.target.value) } value={ confirmpw } />
             </div>
             <input type="submit" value="Create User" />
+            <div>
+                <p>Your Form Data</p>
+                <p>First Name: { firstname }</p>
+                <p>Last Name: { lastname }</p>
+                <p>Email: { email }</p>
+                <p>Password: { password }</p>
+                <p>Confirm Password: { confirmpw }</p>
+            </div>
         </form>
     );
 };
