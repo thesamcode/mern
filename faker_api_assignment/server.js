107be2bad3eeee85e46b51b3ee7a9beccb66f43b
thesamcode <stores1@digitalmail.one>
2022-09-13 15:42:19 -0700

    new updates

---
+++ faker_api_assignment/server.js
@@ -0,0 +1,101 @@
+// Changed to Robert's
+// import { faker } from '@faker-js/faker';
+const {faker} = require("@faker-js/faker")
+
+const express = require("express");
+const app = express();
+
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+const port = 8000;
+
+const createUser = () => {
+    const newUser = {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phone: faker.phone.number(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        userId: faker.datatype.uuid(),
+    };
+    return newUser;
+};
+    
+const newUser = createUser();
+console.log(newUser);
+
+const createCompany = () => {
+    const newCompany = {
+        userId: faker.datatype.uuid(),
+        company: faker.company.name(),
+        street: faker.address.street(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zip: faker.address.zipCode(),
+        country: faker.address.country(),
+    };
+    return newCompany;
+};
+    
+const newCompany = createCompany();
+console.log(newCompany);
+
+
+/*
+ * The output of the above console log will look like this
+ * {
+ *   name: 'Anime Figure',
+ *   price: '$568.00
+ *   department: 'Tools' 
+ * }
+ */
+
+app.post("/api/users/new", (req, res) => {
+    // req.body will contain the form data from Postman or from React
+    console.log(req.body.createUser());
+    // we can push it into the users array for now...
+    // later on this will be inserted into a database
+    users.push(req.body.createUser());
+    // we always need to respond with something
+    res.json( { status: "ok" } );
+});
+
+app.get("/api/users/new", (req, res) => {
+    // res.json({ message: "Hello World" });
+    res.json( newUser );
+});
+
+app.post("/api/companies/new", (req, res) => {
+    // req.body will contain the form data from Postman or from React
+    console.log(req.body.createCompany());
+    // we can push it into the users array for now...
+    // later on this will be inserted into a database
+    companies.push(req.body.createCompany());
+    // we always need to respond with something
+    res.json( { status: "ok" } );
+});
+
+app.get("/api/companies/new", (req, res) => {
+    // res.json({ message: "Hello World" });
+    res.json( newCompany );
+});
+
+app.post("/api/user/company", (req, res) => {
+    // req.body will contain the form data from Postman or from React
+    console.log(req.body.createCompany(), req.body.createUser());
+    // we can push it into the users array for now...
+    // later on this will be inserted into a database
+    companies.push(req.body.createCompany());
+    users.push(req.body.createUser());
+    // we always need to respond with something
+    res.json( { status: "ok" } );
+});
+
+app.get("/api/user/company", (req, res) => {
+    // res.json({ message: "Hello World" });
+    // res.json( newCompany );
+    res.json( newCompany, newUser );
+});
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
