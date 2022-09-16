310ba0a67da8de4069a73845ac8074a251cfcba7
thesamcode <stores1@digitalmail.one>
2022-09-15 17:00:32 -0700

    new updates

--- product_manager_assignment/server.js
+++ product_manager_assignment/server.js
@@ -1,9 +1,13 @@
 
 const express = require('express');
 const cors = require('cors')
-
 const app = express();
+
+require('./server/config/mongoose.config');
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 require('./server/routes/product.routes')(app);
 
