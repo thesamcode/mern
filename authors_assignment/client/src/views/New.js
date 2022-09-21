57f2fac21c430e1b98fc29d532acfc8434aae216
thesamcode <stores1@digitalmail.one>
2022-09-21 15:38:30 -0700

    new updates

--- authors_assignment/client/src/views/New.js
+++ authors_assignment/client/src/views/New.js
@@ -4,16 +4,26 @@ import { Link } from "react-router-dom";
 
 import axios from 'axios';
 
-export default () => {
+export default function Main() {
     const [name, setName] = useState("");
+    const [errors, setErrors] = useState([]);
 
     const onSubmitHandler = e => {
         e.preventDefault();
         axios.post('http://localhost:8000/api/authors', {
-            name,
+            name
         })
             .then(res => console.log(res))
-            .catch(err => console.log(err))
+            // .catch(err => console.log(err))
+            .catch(err=>{
+                const errorResponse = err.response.data.errors; // Get the errors from err.response.data
+                const errorArr = []; // Define a temp error array to push the messages in
+                for (const key of Object.keys(errorResponse)) { // Loop through all errors and get the messages
+                    errorArr.push(errorResponse[key].message)
+                }
+                // Set Errors
+                setErrors(errorArr);
+            })
     }
 
     return (
@@ -24,6 +34,7 @@ export default () => {
             </Link></p>
             <h2>Add a new author: </h2>
             <form onSubmit={onSubmitHandler}>
+            {errors.map((err, index) => <p key={index}>{err}</p>)}
                 <p>
                     <label>Name: </label>
                     <input type="text" onChange={e => setName(e.target.value)} />
